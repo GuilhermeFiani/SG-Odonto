ea84927657ae2d1991967cfbe0f8b404b5d31038
GuilhermeFiani <gfiani10@gmail.com>
2022-04-06 16:48:36 -0400

    Comecando a mexer com migrations

--- app/Http/Controllers/DentistController.php
+++ app/Http/Controllers/DentistController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Dentist;
 
 class DentistController extends Controller
 {
     public function index(){
         return view('dentist.dashboard');
+    }
+    public function show() {
+        return Dentist::all();
     }
 }
---
+++ database/migrations/2022_04_06_190428_create_patients_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('residence')->nullable();
+            $table->string('address');
+            $table->string('job')->nullable();
+            $table->date('birthday');
+            $table->date('starttrat');
+            $table->date('endtrat')->nullable();
+            $table->timestamps();
+        });
+
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+};
